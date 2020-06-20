000fd1128cfbd4761d8622ed17e73b77c6395d8e
Math M <mdmoriss@ucsd.edu>
2020-06-19 17:13:33 -0700

    Endless Mode scoreboard

---
+++ Assets/endlessScore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class endlessScore : MonoBehaviour
+{
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GameManager.endlessMode == true)
+        {
+            scoreText.enabled = true;
+        }
+        else
+        {
+            scoreText.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        scoreText.text = "Score: " + GameManager.score;
+    }
+}
