098c5f95dad3284ffe30e07e7d92a6602e3d65c9
Math M <mdmoriss@ucsd.edu>
2020-06-25 16:04:11 -0700

    Difficulty Slider

---
+++ Assets/difficultySlider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class difficultySlider : MonoBehaviour
+{
+    int difficulty;
+    public Slider difficultySetter;
+    public Text difficultyText;
+
+    private void Start()
+    {
+        difficulty = 0;
+        PlayerPrefs.SetInt("Difficulty", difficulty);
+    }
+
+    public void setDifficulty()
+    {
+        difficulty = (int)Mathf.Round(difficultySetter.value);
+        if(difficulty == 0)
+        {
+            difficultyText.text = "Easy";
+        }
+        else if(difficulty == 1)
+        {
+            difficultyText.text = "Medium";
+        }
+        else
+        {
+            difficultyText.text = "Hard";
+        }
+        PlayerPrefs.SetInt("Difficulty", difficulty);
+    }
+}
