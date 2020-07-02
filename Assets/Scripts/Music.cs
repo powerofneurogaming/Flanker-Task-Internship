1ac2897eebf69c7a36ef205bb7909ff21e7f40c8
SETUP18 <66759206+SETUP18@users.noreply.github.com>
2020-07-02 13:34:05 -0700

    Added Title Screen Music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+	public AudioClip music;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundManager.loop = true;
+		SoundManager.Instance.audioSource.Play(music, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
