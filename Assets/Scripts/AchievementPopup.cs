6077d87fe9ccda25e4045a14cd37679d81b5e325
Math M <mdmoriss@ucsd.edu>
2020-08-06 14:08:10 -0700

    Achievement ding sound

--- Assets/Scripts/AchievementPopup.cs
+++ Assets/Scripts/AchievementPopup.cs
@@ -9,6 +9,11 @@ public class AchievementPopup : MonoBehaviour
     // !! IS NOT PERSISTENT ACROSS MULTIPLE SCENES !!
     public static AchievementPopup Instance { get; private set; }
 
+    AudioSource sfxSource;
+
+    [SerializeField]
+    AudioClip ding;
+
     private void Awake()
     {
         if (Instance == null)
@@ -43,6 +48,8 @@ public class AchievementPopup : MonoBehaviour
 
     private void Start()
     {
+        sfxSource = SoundManager.Instance.audioSource;
+
         achAnim = achievePanel.GetComponent<Animator>();
         achAnim.StopPlayback();
         if (AchievementManager.Instance.achTrigger.Count != 0)
@@ -58,6 +65,7 @@ public class AchievementPopup : MonoBehaviour
             medal.sprite = medals[AchievementManager.Instance.achTrigger[0].state];
             achText.text = AchievementManager.Instance.achTrigger[0].friendlyName;
             achTier.text = levels[AchievementManager.Instance.achTrigger[0].state];
+            sfxSource.PlayOneShot(ding, stateManager.Instance.volume);
             achAnim.Play("slide-in");
             achAnim.StopPlayback();
             yield return new WaitForSeconds(3.0f);
